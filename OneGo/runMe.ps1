2b083b748a9f5f25803c87cc02709812cd46878f
Mohammad Hossein Amri <amrimh@outlook.com>
2018-10-28 22:45:24 +0800

    add git push to OneGo script

--- OneGo/runMe.ps1
+++ OneGo/runMe.ps1
@@ -1,15 +1,13 @@
 $date= Get-Date;
 Write-Output "started...";
 for ($i = $date.AddMonths(-12); $i -le $date; $i=$i.AddDays(1)) {
-    
     $randomCommit=Get-Random -Maximum 50 -Minimum 1;
     Set-Date $i;
     for ($j = 0; $j -lt $randomCommit ; $j++) {
         Write-Output "$i $j" | Out-File dummy.txt -NoNewline
         cmd /c "git add .";
-        # cmd /c "dir";
         cmd /c "git commit -a -m ""add file to git $i $j""";
     }
-    
 }
+cmd /c "git push";
 Write-Output "Ended...";
